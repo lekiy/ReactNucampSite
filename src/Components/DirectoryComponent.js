fd32aa797c5057ab61b44f53e539c5eb7c8c14f5
Kevin Annen <lekiyminin@gmail.com>
2020-06-05 23:04:07 -0500

    added Breadcrumbs

--- src/Components/DirectoryComponent.js
+++ src/Components/DirectoryComponent.js
@@ -1,54 +1,16 @@
 import React, {Component} from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Form} from 'reactstrap';
+import {Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
-// class Directory extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             selectedCampsite: null
-//         };
-//     }
-
-//     onCampsiteSelect(campsite){
-//         this.setState({selectedCampsite: campsite});
-//     }
-
-//     render(){
-//         const directory = this.props.campsites.map(campsite => {
-//             return (
-//                 <div key={campsite.id} className = "col-md-5 m-1">
-//                     <Card onClick={() => this.props.onClick(campsite.id)}>
-//                         <CardImg width="100%" src={campsite.image} alt = {campsite.name} />
-//                         <CardImgOverlay>
-//                             <CardImgOverlay>
-//                                 <CardTitle>{campsite.name}</CardTitle>
-//                             </CardImgOverlay>
-//                         </CardImgOverlay>
-//                     </Card>
-//                 </div>
-//             );
-//         });
-
-//         return (
-//             <div className="container">
-//                 <div className="row">
-//                     {directory}
-//                 </div>
-//             </div>
-//         );
-
-//     }
-
-    
-// }
-
-function RenderDirectoryItem({campsite, onClick}){
+function RenderDirectoryItem({campsite}){
     return (
         <Card>
-            <CardImg width="100" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     )
 }
@@ -64,6 +26,16 @@ function Directory(props){
 
     return (
         <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
+            </div>
             <div className="row">
                 {directory}
             </div>
